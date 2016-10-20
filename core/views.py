20ba64e8fdbaa306e4c6ec1a964d6d34fde01515
ma0 <ma0@contraslash.com>
2016-10-20 10:37:16 -0500

    solved issues with templates

--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    return render(request,'core/dashboard/index.html')
+    return render(request, 'core/dashboard/index.html')
 
 
 @login_required
